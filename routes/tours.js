383b03699ca5f6fceb66aab03e1e7590888ab2a3
Cam <github@lekienhoanh.dev>
2022-08-30 17:55:10 +0700

    update CRUD permission for tours

--- routes/tours.js
+++ routes/tours.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
 const controller = require("../controllers/tour");
+const {
+	canViewTour,
+	scopedTours,
+	canDeleteTour,
+} = require("../permissions/tour");
 
-router.get("/", auth, controller.index);
-router.get("/:id", auth, controller.show);
-router.post("/", auth, controller.create);
-router.put("/:id", auth, controller.update);
-router.delete("/:id", auth, controller.delete);
+router.get("/", controller.index);
+router.get("/:id", authGetTour, controller.show);
+router.post("/", controller.create);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.delete);
+
+function authGetTour(req, res, next) {
+	console.log(canViewTour(req.user));
+	if (!canViewTour(req.user)) {
+		return res.status(403).json({ message: "Forbidden" });
+	}
+
+	next();
+}
 
 module.exports = router;
