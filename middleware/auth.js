ec01b157efb81175a8c5f7fa0a3c0ed33ffed0c6
Restyled.io <commits@restyled.io>
2022-08-31 02:27:21 +0000

    Restyled by clang-format

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,22 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-	try {
-		const token = req.headers.authorization.split(" ")[1];
+  try {
+    const token = req.headers.authorization.split(" ")[1];
 
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-		const userId = decoded.userId;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.userId;
 
-		if (req.body.userId && req.body.userId !== userId) {
-			return res.status(403).json({ message: "User ID is not valid" });
-		}
+    if (req.body.userId && req.body.userId !== userId) {
+      return res.status(403).json({message : "User ID is not valid"});
+    }
 
-		req.user = decoded;
+    req.user = decoded;
 
-		next();
-	} catch (err) {
-		return res.status(403).json({
-			error: "Please login.",
-		});
-	}
+    next();
+  } catch (err) {
+    return res.status(403).json({
+      error : "Please login.",
+    });
+  }
 };
--- permissions/tour.js
+++ permissions/tour.js
@@ -1,26 +1,22 @@
 const canViewTour = (user, tour) => {
-	return user.roleId === 1 || user.roleId === 2 || tour.owner === user.id;
+  return user.roleId === 1 || user.roleId === 2 || tour.owner === user.id;
 };
 
 const scopedTours = (user, tours) => {
-	if (user.roleId === "1") {
-		return tours;
-	}
+  if (user.roleId === "1") {
+    return tours;
+  }
 
-	return tours.filter((tour) => tour.owner === user.id);
+  return tours.filter((tour) => tour.owner === user.id);
 };
 
-const canDeleteTour = (user, tour) => {
-	return tour.owner === user.id;
-};
+const canDeleteTour = (user, tour) => { return tour.owner === user.id; };
 
-const canEditTour = (user, tour) => {
-	return tour.owner === user.id;
-};
+const canEditTour = (user, tour) => { return tour.owner === user.id; };
 
 module.exports = {
-	canViewTour,
-	scopedTours,
-	canDeleteTour,
-	canEditTour,
+  canViewTour,
+  scopedTours,
+  canDeleteTour,
+  canEditTour,
 };
