d8284f819597decc46afff356ad5f0021f46ca1e
Cam <github@lekienhoanh.dev>
2022-09-06 16:49:48 +0700

    update relationship

--- models/city.js
+++ models/city.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			// define association here
+			models.Country.belongsTo(models.City, {
+				foreignKey: "countryId",
+				targetKey: "id",
+			});
 		}
 	}
 	City.init(
--- models/user.js
+++ models/user.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
 		 */
 		static associate(models) {
 			// define association here
+			models.Role.belongsTo(models.Section, {
+				foreignKey: "roleId",
+				targetKey: "id",
+			});
 		}
 	}
 	User.init(
