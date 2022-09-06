f9106313f86d869a931c8347e737c2798388031f
Restyled.io <commits@restyled.io>
2022-09-06 03:58:26 +0000

    Restyled by prettier

--- routes/tours.js
+++ routes/tours.js
@@ -11,13 +11,13 @@ router.put("/:id", setTour, permission, controller.update);
 router.delete("/:id", setTour, permission, controller.delete);
 
 async function setTour(req, res, next) {
-  const tour = await models.Tour.findOne({where : {id : req.params.id}});
-  if (!tour) {
-    res.status(404).json({message : "Tour not found"});
-  }
+	const tour = await models.Tour.findOne({ where: { id: req.params.id } });
+	if (!tour) {
+		res.status(404).json({ message: "Tour not found" });
+	}
 
-  req.item = tour.dataValues;
-  next();
+	req.item = tour.dataValues;
+	next();
 }
 
 module.exports = router;
