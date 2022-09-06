a7aa7ffa5bd7d393b1c75e2867b2e1ca86e820bd
Cam <github@lekienhoanh.dev>
2022-09-06 17:40:10 +0700

    added new role and permission

--- config/data.js
+++ config/data.js
@@ -2,19 +2,22 @@ const ROLE = {
 	ADMIN: 1,
 	OWNER: 2,
 	USER: 3,
+	EDITOR: 4,
 };
 
 const PERMISSION = {
 	CREATE: 1,
 	UPDATE: 2,
 	DELETE: 3,
+	ACTIVE: 4,
 };
 
 const PERMISSION_ROLE = {
-	READ: [ROLE.ADMIN, ROLE.OWNER],
-	CREATE: [ROLE.ADMIN, ROLE.OWNER],
-	UPDATE: [ROLE.ADMIN, ROLE.OWNER],
-	DELETE: [ROLE.OWNER],
+	READ: { role: [ROLE.ADMIN, ROLE.OWNER], field: null },
+	CREATE: { role: [ROLE.ADMIN, ROLE.OWNER], field: null },
+	UPDATE: { role: [ROLE.ADMIN, ROLE.OWNER], field: null },
+	DELETE: { role: [ROLE.OWNER], field: null },
+	ACTIVE: { role: [ROLE.EDITOR], field: ["active"] },
 };
 
 module.exports = {
