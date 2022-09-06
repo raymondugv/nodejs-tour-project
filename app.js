62426e2d38e6a2293777b64e1d414a75aa5ea6fc
Restyled.io <commits@restyled.io>
2022-09-06 03:53:30 +0000

    Restyled by prettier

--- app.js
+++ app.js
@@ -7,23 +7,23 @@ require("dotenv").config();
 const auth = require("./middleware/auth");
 const app = express();
 
-const whitelist = [ "http://localhost:3000", "http://localhost:8000" ];
+const whitelist = ["http://localhost:3000", "http://localhost:8000"];
 const corsOptions = {
-  origin : (origin, callback) => {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials : true,
+	origin: (origin, callback) => {
+		if (!origin || whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	credentials: true,
 };
 
 app.use(cors(corsOptions));
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
--- routes/roles-data.js
+++ routes/roles-data.js
@@ -1,32 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const {ROLE, PERMISSION, PERMISSION_ROLE} = require("../data");
+const { ROLE, PERMISSION, PERMISSION_ROLE } = require("../data");
 const {
-  canViewItem,
-  canDeleteItem,
-  canEditItem,
+	canViewItem,
+	canDeleteItem,
+	canEditItem,
 } = require("../permissions/general");
 
-router.get("/",
-           (req, res,
-            next) => { return res.json({ROLE, PERMISSION, PERMISSION_ROLE}); });
+router.get("/", (req, res, next) => {
+	return res.json({ ROLE, PERMISSION, PERMISSION_ROLE });
+});
 
-router.get("/browse",
-           (req, res,
-            next) => { return res.json(PERMISSION_ROLE.BROWSE.includes(1)); });
+router.get("/browse", (req, res, next) => {
+	return res.json(PERMISSION_ROLE.BROWSE.includes(1));
+});
 
 router.get("/test", (req, res, next) => {
-  const condition = canViewItem(req.user, 5) ||
-                    canEditItem(req.user, req.item) ||
-                    canDeleteItem(req.user, req.item);
+	const condition =
+		canViewItem(req.user, 5) ||
+		canEditItem(req.user, req.item) ||
+		canDeleteItem(req.user, req.item);
 
-  if (!condition) {
-    return res.status(403).json({
-      error : "You do not have permission to perform this action.",
-    });
-  }
+	if (!condition) {
+		return res.status(403).json({
+			error: "You do not have permission to perform this action.",
+		});
+	}
 
-  return res.json({condition});
+	return res.json({ condition });
 });
 
 module.exports = router;
