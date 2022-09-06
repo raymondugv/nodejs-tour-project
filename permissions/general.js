f293e0c8742fa95cd964317e4b533d8e0d45d014
Restyled.io <commits@restyled.io>
2022-09-06 10:11:22 +0000

    Restyled by clang-format

--- permissions/general.js
+++ permissions/general.js
@@ -1,31 +1,29 @@
-const { ROLE, PERMISSION_ROLE } = require("../config/data");
-const isOwner = (user, item) => {
-	return item.owner === user.id;
-};
+const {ROLE, PERMISSION_ROLE} = require("../config/data");
+const isOwner = (user, item) => { return item.owner === user.id; };
 
 const canViewItem = (user, item) => {
-	return PERMISSION_ROLE.READ.includes(user.roleId) || isOwner(user, item);
+  return PERMISSION_ROLE.READ.includes(user.roleId) || isOwner(user, item);
 };
 
 const scopedItems = (user, items) => {
-	if (user.roleId === ROLE.ADMIN) {
-		return items;
-	}
+  if (user.roleId === ROLE.ADMIN) {
+    return items;
+  }
 
-	return items.filter((item) => isOwner(user, item));
+  return items.filter((item) => isOwner(user, item));
 };
 
 const canDeleteItem = (user, item) => {
-	return PERMISSION_ROLE.DELETE.includes(user.roleId) || isOwner(user, item);
+  return PERMISSION_ROLE.DELETE.includes(user.roleId) || isOwner(user, item);
 };
 
 const canEditItem = (user, item) => {
-	return PERMISSION_ROLE.UPDATE.includes(user.roleId) || isOwner(user, item);
+  return PERMISSION_ROLE.UPDATE.includes(user.roleId) || isOwner(user, item);
 };
 
 module.exports = {
-	canViewItem,
-	scopedItems,
-	canDeleteItem,
-	canEditItem,
+  canViewItem,
+  scopedItems,
+  canDeleteItem,
+  canEditItem,
 };
