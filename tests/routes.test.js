2798c80308ac610b97bc19c8d385fc3322c6b668
Cam <github@lekienhoanh.dev>
2022-09-07 17:42:08 +0700

    update test cases

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,12 +1,12 @@
 const request = require("supertest");
 const app = require("../app");
+let token = "";
 
-test("Login endpoint", async () => {
-	const res = await request(app).post("/login").send({
+beforeAll(async () => {
+	const response = await request(app).post("/login").send({
 		email: "admin@admin.com",
 		password: "admin",
 	});
 
-	expect(res.statusCode).toEqual(200);
-	expect(res.body).toHaveProperty("token");
+	token = response.body.token;
 });
