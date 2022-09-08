f5ec9ea53b6a8dd4750e12d9d620d2c218851a5c
Cam <github@lekienhoanh.dev>
2022-09-08 16:53:37 +0700

    update country test

--- tests/countries.test.js
+++ tests/countries.test.js
@@ -6,7 +6,11 @@ const { login, logout } = require("../tests/auth-test");
 let token = "";
 
 const CREATE_PAYLOAD = {
-	name: "New test country 2",
+	name: "New test country",
+};
+
+const UPDATE_PAYLOAD = {
+	name: "Update test country",
 };
 
 describe("countries function with admin role", () => {
@@ -16,9 +20,43 @@ describe("countries function with admin role", () => {
 		token = response.body.token;
 	});
 
-	test("get countries list", async () => {
+	test("get Country list", async () => {
 		const response = await request(app)
 			.get("/countries")
+			.set("Authorization", `Bearer ${token}`);
+
+		expect(response.statusCode).toBe(200);
+	});
+
+	test("get Country detail", async () => {
+		const response = await request(app)
+			.get("/countries/1")
+			.set("Authorization", `Bearer ${token}`);
+
+		expect(response.statusCode).toBe(200);
+	});
+
+	test("create new Country", async () => {
+		const response = await request(app)
+			.post("/countries")
+			.set("Authorization", `Bearer ${token}`)
+			.send(CREATE_PAYLOAD);
+
+		expect(response.statusCode).toBe(201);
+	});
+
+	test("update Country", async () => {
+		const response = await request(app)
+			.put("/countries/1")
+			.set("Authorization", `Bearer ${token}`)
+			.send(UPDATE_PAYLOAD);
+
+		expect(response.statusCode).toBe(200);
+	});
+
+	test("delete Country", async () => {
+		const response = await request(app)
+			.delete("/countries/4")
 			.set("Authorization", `Bearer ${token}`);
 
 		expect(response.statusCode).toBe(200);
