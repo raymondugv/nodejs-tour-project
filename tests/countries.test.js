1130f533d27b82c153aba1f77f4db8032a8803fc
Cam <github@lekienhoanh.dev>
2022-09-08 16:45:32 +0700

    create coutries test

---
+++ tests/countries.test.js
@@ -0,0 +1,31 @@
+const request = require("supertest");
+const app = require("../app");
+
+const { login, logout } = require("../tests/auth-test");
+
+let token = "";
+
+const CREATE_PAYLOAD = {
+	name: "New test country 2",
+};
+
+describe("countries function with admin role", () => {
+	beforeAll(async () => {
+		const response = await login("admin@email.com", "admin");
+
+		console.log("response", response.body);
+		token = response.body.token;
+	});
+
+	test("get countries list", async () => {
+		const response = await request(app)
+			.get("/countries")
+			.set("Authorization", `Bearer ${token}`);
+
+		expect(response.statusCode).toBe(200);
+	});
+
+	afterAll(async () => {
+		await logout(token).then((token = ""));
+	});
+});
