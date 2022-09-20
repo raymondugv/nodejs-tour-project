48869687438be7ed8ae40ab59a529ea7ad265393
Cam <github@lekienhoanh.dev>
2022-09-20 10:46:31 +0700

    update Tour's API route

--- routes/tours.js
+++ routes/tours.js
@@ -3,19 +3,20 @@ const router = express.Router();
 const controller = require("../controllers/tour");
 const verifyRoles = require("../middleware/permission");
 const multer = require("multer");
+const storage = require("../config/uploadConfig");
 
 router.get("/", controller.index);
 router.get("/:id", verifyRoles, controller.show);
 router.post(
 	"/",
 	verifyRoles,
-	multer({ dest: "public/images/" }).single("image"),
+	multer({ storage: storage }).single("image"),
 	controller.create
 );
 router.put(
 	"/:id",
 	verifyRoles,
-	multer({ dest: "public/images/" }).single("image"),
+	multer({ storage: storage }).single("image"),
 	controller.update
 );
 router.delete("/:id", verifyRoles, controller.delete);
