c7c2e133f49c16b3fa4ac3cd87a451dd054642ac
Cam <github@lekienhoanh.dev>
2022-09-21 14:54:56 +0700

    added booking informations route

---
+++ routes/booking-informations.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const verifyRoles = require("../middleware/permission");
+const controllers = require("../controllers/booking-informations");
+
+router.get("/", controllers.index);
+router.get("/:id", verifyRoles, controllers.show);
+router.post("/", verifyRoles, controllers.create);
+router.put("/:id", verifyRoles, controllers.update);
+router.delete("/:id", verifyRoles, controllers.delete);
+
+module.exports = router;
