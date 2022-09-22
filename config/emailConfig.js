3755c1e147c6b4d8666d8eecbc9675ac6b0b0555
Cam <github@lekienhoanh.dev>
2022-09-22 16:03:08 +0700

    update EMAIL CONFIG

---
+++ config/emailConfig.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const emailConfig = {
+	host: process.env.EMAIL_HOST,
+	port: process.env.EMAIL_PORT,
+	secure: process.env.EMAIL_SECURE,
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASSWORD,
+	},
+	ignoreTLS: false,
+};
+
+const from_address = process.env.FROM_ADDRESS;
+
+module.exports = { emailConfig, from_address };
