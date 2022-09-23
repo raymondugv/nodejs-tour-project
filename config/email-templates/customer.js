df07b713f36ab80b067106f4d9ff4cd70c4bd991
Cam <github@lekienhoanh.dev>
2022-09-23 17:45:26 +0700

    update customer Email template

--- config/email-templates/customer.js
+++ config/email-templates/customer.js
@@ -26,4 +26,38 @@ const bookingUpdateForCustomer = (booking, customer) => `
 	<p><strong>Updated At:</strong> ${booking.updatedAt}</p>
 `;
 
-module.exports = { newBookingForCustomer, bookingUpdateForCustomer };
+const bookingCancleForCustomer = (booking) => `
+<p><strong>Booking Number:</strong> ${booking.booking_number}</p>
+<p><strong>Customer Name:</strong> ${customer.name}</p>
+<p><strong>Customer Email:</strong> ${customer.email}</p>
+<p><strong>Customer Phone:</strong> ${customer.phone}</p>
+
+<p><strong>Booking Status:</strong> ${booking.booking.name}</p>
+<p><strong>Payment Status:</strong> ${booking.payment.name}</p>
+
+<p><strong>Number of Pax:</strong> ${booking.number_of_pax}</p>
+<p><strong>Departure Date:</strong> ${booking.departure_date}</p>
+
+<p><strong>Updated At:</strong> ${booking.updatedAt}</p>`;
+
+const bookingPaymentUpdateForCustomer = (booking) => `
+<p><strong>Booking Number:</strong> ${booking.booking_number}</p>
+<p><strong>Customer Name:</strong> ${customer.name}</p>
+<p><strong>Customer Email:</strong> ${customer.email}</p>
+<p><strong>Customer Phone:</strong> ${customer.phone}</p>
+
+<p><strong>Booking Status:</strong> ${booking.booking.name}</p>
+<p><strong>Payment Status:</strong> ${booking.payment.name}</p>
+
+<p><strong>Number of Pax:</strong> ${booking.number_of_pax}</p>
+<p><strong>Departure Date:</strong> ${booking.departure_date}</p>
+
+<p><strong>Updated At:</strong> ${booking.updatedAt}</p>
+`;
+
+module.exports = {
+	newBookingForCustomer,
+	bookingUpdateForCustomer,
+	bookingCancleForCustomer,
+	bookingPaymentUpdateForCustomer,
+};
