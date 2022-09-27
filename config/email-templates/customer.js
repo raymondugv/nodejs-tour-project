e04e08cfa6c6e7c9648f50fef90d0cc5c5cbc5e5
Cam <github@lekienhoanh.dev>
2022-09-27 14:39:38 +0700

    update Customer email config

--- config/email-templates/customer.js
+++ config/email-templates/customer.js
@@ -27,32 +27,33 @@ const bookingUpdateForCustomer = (booking, customer) => `
 `;
 
 const bookingCancleForCustomer = (booking) => `
-<p><strong>Booking Number:</strong> ${booking.booking_number}</p>
-<p><strong>Customer Name:</strong> ${customer.name}</p>
-<p><strong>Customer Email:</strong> ${customer.email}</p>
-<p><strong>Customer Phone:</strong> ${customer.phone}</p>
+	<p><strong>Booking Number:</strong> ${booking.booking_number}</p>
+	<p><strong>Customer Name:</strong> ${customer.name}</p>
+	<p><strong>Customer Email:</strong> ${customer.email}</p>
+	<p><strong>Customer Phone:</strong> ${customer.phone}</p>
 
-<p><strong>Booking Status:</strong> ${booking.booking.name}</p>
-<p><strong>Payment Status:</strong> ${booking.payment.name}</p>
+	<p><strong>Booking Status:</strong> ${booking.booking.name}</p>
+	<p><strong>Payment Status:</strong> ${booking.payment.name}</p>
 
-<p><strong>Number of Pax:</strong> ${booking.number_of_pax}</p>
-<p><strong>Departure Date:</strong> ${booking.departure_date}</p>
+	<p><strong>Number of Pax:</strong> ${booking.number_of_pax}</p>
+	<p><strong>Departure Date:</strong> ${booking.departure_date}</p>
 
-<p><strong>Updated At:</strong> ${booking.updatedAt}</p>`;
+	<p><strong>Updated At:</strong> ${booking.updatedAt}</p>
+`;
 
 const bookingPaymentUpdateForCustomer = (booking) => `
-<p><strong>Booking Number:</strong> ${booking.booking_number}</p>
-<p><strong>Customer Name:</strong> ${customer.name}</p>
-<p><strong>Customer Email:</strong> ${customer.email}</p>
-<p><strong>Customer Phone:</strong> ${customer.phone}</p>
+	<p><strong>Booking Number:</strong> ${booking.booking_number}</p>
+	<p><strong>Customer Name:</strong> ${customer.name}</p>
+	<p><strong>Customer Email:</strong> ${customer.email}</p>
+	<p><strong>Customer Phone:</strong> ${customer.phone}</p>
 
-<p><strong>Booking Status:</strong> ${booking.booking.name}</p>
-<p><strong>Payment Status:</strong> ${booking.payment.name}</p>
+	<p><strong>Booking Status:</strong> ${booking.booking.name}</p>
+	<p><strong>Payment Status:</strong> ${booking.payment.name}</p>
 
-<p><strong>Number of Pax:</strong> ${booking.number_of_pax}</p>
-<p><strong>Departure Date:</strong> ${booking.departure_date}</p>
+	<p><strong>Number of Pax:</strong> ${booking.number_of_pax}</p>
+	<p><strong>Departure Date:</strong> ${booking.departure_date}</p>
 
-<p><strong>Updated At:</strong> ${booking.updatedAt}</p>
+	<p><strong>Updated At:</strong> ${booking.updatedAt}</p>
 `;
 
 module.exports = {
