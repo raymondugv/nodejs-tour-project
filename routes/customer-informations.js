cd9cc63168230c0e1cf8bd69f6e7484b7a7713b0
Cam <github@lekienhoanh.dev>
2022-10-03 11:09:48 +0700

    update customer route

--- routes/customer-informations.js
+++ routes/customer-informations.js
@@ -2,11 +2,23 @@ const express = require("express");
 const router = express.Router();
 const verifyRoles = require("../middleware/permission");
 const controllers = require("../controllers/customer-information");
+const multer = require("multer");
+const storage = require("../config/uploadConfig");
 
 router.get("/", controllers.index);
 router.get("/:id", verifyRoles, controllers.show);
-router.post("/", verifyRoles, controllers.create);
-router.put("/:id", verifyRoles, controllers.update);
+router.post(
+	"/",
+	verifyRoles,
+	multer({ storage: storage }).single("avatar"),
+	controllers.create
+);
+router.put(
+	"/:id",
+	verifyRoles,
+	multer({ storage: storage }).single("avatar"),
+	controllers.update
+);
 router.delete("/:id", verifyRoles, controllers.delete);
 
 module.exports = router;
