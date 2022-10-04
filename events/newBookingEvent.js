6c74363709766ff12890226dac57a117d5d31cf2
Cam <github@lekienhoanh.dev>
2022-10-04 10:38:25 +0700

    update new booking event

--- events/newBookingEvent.js
+++ events/newBookingEvent.js
@@ -7,38 +7,30 @@ const exchange = require("../config/currencyTransfer");
 const newBookingEvent = eventEmitter.addListener(
 	"booking.created",
 	async (booking) => {
-		sendEmail(
+		const fields = {
+			bookingNumber: booking.booking_number,
+			createdAt: booking.createdAt,
+			customerName: booking.customer.name,
+			customerEmail: booking.customer.email,
+			customerPhone: booking.customer.phone,
+			tourTitle: booking.tour.title,
+			tourDescription: booking.tour.description,
+			numberOfPax: booking.number_of_pax,
+			tourPrice: exchange(booking.tour.price),
+		};
+
+		await sendEmail(
 			"staff@nodetour.js",
 			"New Booking received" + booking.booking_number,
 			"../config/email-templates/newBookingStaff.html",
-			{
-				bookingNumber: booking.booking_number,
-				createdAt: booking.createdAt,
-				customerName: booking.customer.name,
-				customerEmail: booking.customer.email,
-				customerPhone: booking.customer.phone,
-				tourTitle: booking.tour.title,
-				tourDescription: booking.tour.description,
-				numberOfPax: booking.number_of_pax,
-				tourPrice: exchange(booking.tour.price),
-			}
+			fields
 		);
 
-		sendEmail(
+		await sendEmail(
 			"customer@nodetour.js",
 			"New Booking Customer" + booking.booking_number,
 			"../config/email-templates/newBookingStaff.html",
-			{
-				bookingNumber: booking.booking_number,
-				createdAt: booking.createdAt,
-				customerName: booking.customer.name,
-				customerEmail: booking.customer.email,
-				customerPhone: booking.customer.phone,
-				tourTitle: booking.tour.title,
-				tourDescription: booking.tour.description,
-				numberOfPax: booking.number_of_pax,
-				tourPrice: exchange(booking.tour.price),
-			}
+			fields
 		);
 	}
 );
