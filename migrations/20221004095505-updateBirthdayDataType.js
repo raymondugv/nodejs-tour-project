7636bc011f699c5a7ade1b43ea55946685ce86db
Restyled.io <commits@restyled.io>
2022-10-04 10:07:30 +0000

    Restyled by clang-format

--- migrations/20221004095505-updateBirthdayDataType.js
+++ migrations/20221004095505-updateBirthdayDataType.js
@@ -1,27 +1,27 @@
 "use strict";
 
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		/**
-		 * Add altering commands here.
-		 *
-		 * Example:
-		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-		 */
-		await queryInterface.changeColumn("CustomerInformations", "birthday", {
-			type: Sequelize.DATEONLY,
-		});
-	},
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.changeColumn("CustomerInformations", "birthday", {
+      type : Sequelize.DATEONLY,
+    });
+  },
 
-	async down(queryInterface, Sequelize) {
-		/**
-		 * Add reverting commands here.
-		 *
-		 * Example:
-		 * await queryInterface.dropTable('users');
-		 */
-		await queryInterface.changeColumn("CustomerInformations", "birthday", {
-			type: Sequelize.DATE,
-		});
-	},
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.changeColumn("CustomerInformations", "birthday", {
+      type : Sequelize.DATE,
+    });
+  },
 };
