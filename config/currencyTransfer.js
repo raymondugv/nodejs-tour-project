bd66a92dd62b47acbd2a3951ce808ff481ada7ac
Cam <github@lekienhoanh.dev>
2022-10-12 18:13:59 +0700

    remove old `config` folder

--- config/currencyTransfer.js
+++
@@ -1,3 +0,0 @@
-export const exchange = (amount) => {
-	return amount.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-};
--- config/email-templates/staff.js
+++
@@ -1,8 +0,0 @@
-const newBookingForStaff = (booking, customer) => `
-	<p><strong>Booking Number:</strong> ${booking.booking_number}</p>
-	<p><strong>Customer Name:</strong> ${customer.name}</p>
-	<p><strong>Customer Email:</strong> ${customer.email}</p>
-	<p><strong>Customer Phone:</strong> ${customer.phone}</p>
-`;
-
-module.exports = { newBookingForStaff };
