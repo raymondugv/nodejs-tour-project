0d9d8075fc2368d49695a7f65bf93da437bc1d85
Cam <github@lekienhoanh.dev>
2022-10-12 18:49:46 +0700

    update migrations

---
+++ src/migrations/20220824091359-create-tour.js
@@ -0,0 +1,55 @@
+"use strict";
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("Tours", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			title: {
+				type: Sequelize.STRING,
+			},
+			slug: {
+				type: Sequelize.STRING,
+			},
+			description: {
+				type: Sequelize.TEXT,
+			},
+			image: {
+				type: Sequelize.STRING,
+			},
+			price: {
+				type: Sequelize.DECIMAL,
+			},
+			departure_date: {
+				type: Sequelize.TEXT,
+			},
+			departure: {
+				type: Sequelize.INTEGER,
+			},
+			arrival: {
+				type: Sequelize.INTEGER,
+			},
+			owner: {
+				type: Sequelize.INTEGER,
+			},
+			status: {
+				type: Sequelize.INTEGER,
+				defaultValue: 0,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("Tours");
+	},
+};
