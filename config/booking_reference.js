75ed5ed6ae7c91a4d80544ea2fd851e79a41686c
Cam <github@lekienhoanh.dev>
2022-10-12 16:23:07 +0700

    fix booking_code function

--- config/booking_reference.js
+++ config/booking_reference.js
@@ -1,8 +1,10 @@
 const letters = "ABCDEFGHJKMNPQRSTUXY123456789";
-export const uniqueId = () => {
+function booking_code() {
 	var text = "";
 	for (var i = 0; i < 6; i++) {
 		text += letters.charAt(Math.floor(Math.random() * letters.length));
 	}
 	return text;
-};
+}
+
+export default booking_code;
