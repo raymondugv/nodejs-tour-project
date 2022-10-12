3f66f3dc873fea31cd4225bd53298282fb70e8af
Restyled.io <commits@restyled.io>
2022-10-12 02:43:48 +0000

    Restyled by prettier

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
+	try {
+		const token = req.headers.authorization.split(" ")[1];
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded;
+		req.user = decoded;
 
-    next();
-  } catch (err) {
-    return res.status(403).json({
-      error : "Please login.",
-    });
-  }
+		next();
+	} catch (err) {
+		return res.status(403).json({
+			error: "Please login.",
+		});
+	}
 };
--- tests/countries.test.js
+++ tests/countries.test.js
@@ -1,66 +1,68 @@
 const request = require("supertest");
 const app = require("@app");
 
-const {login, logout} = require("@tests/auth-test");
+const { login, logout } = require("@tests/auth-test");
 
 let token = "";
 
 const CREATE_PAYLOAD = {
-  name : "New test country",
+	name: "New test country",
 };
 
 const UPDATE_PAYLOAD = {
-  name : "Update test country",
+	name: "Update test country",
 };
 
 describe("countries function with admin role", () => {
-  beforeAll(async () => {
-    const response = await login("admin@email.com", "admin");
+	beforeAll(async () => {
+		const response = await login("admin@email.com", "admin");
 
-    token = response.body.token;
-  });
+		token = response.body.token;
+	});
 
-  test("get Country list", async () => {
-    const response = await request(app)
-                         .get("/countries")
-                         .set("Authorization", `Bearer ${token}`);
+	test("get Country list", async () => {
+		const response = await request(app)
+			.get("/countries")
+			.set("Authorization", `Bearer ${token}`);
 
-    expect(response.statusCode).toBe(200);
-  });
+		expect(response.statusCode).toBe(200);
+	});
 
-  test("get Country detail", async () => {
-    const response = await request(app)
-                         .get("/countries/1")
-                         .set("Authorization", `Bearer ${token}`);
+	test("get Country detail", async () => {
+		const response = await request(app)
+			.get("/countries/1")
+			.set("Authorization", `Bearer ${token}`);
 
-    expect(response.statusCode).toBe(200);
-  });
+		expect(response.statusCode).toBe(200);
+	});
 
-  test("create new Country", async () => {
-    const response = await request(app)
-                         .post("/countries")
-                         .set("Authorization", `Bearer ${token}`)
-                         .send(CREATE_PAYLOAD);
+	test("create new Country", async () => {
+		const response = await request(app)
+			.post("/countries")
+			.set("Authorization", `Bearer ${token}`)
+			.send(CREATE_PAYLOAD);
 
-    expect(response.statusCode).toBe(201);
-  });
+		expect(response.statusCode).toBe(201);
+	});
 
-  test("update Country", async () => {
-    const response = await request(app)
-                         .put("/countries/1")
-                         .set("Authorization", `Bearer ${token}`)
-                         .send(UPDATE_PAYLOAD);
+	test("update Country", async () => {
+		const response = await request(app)
+			.put("/countries/1")
+			.set("Authorization", `Bearer ${token}`)
+			.send(UPDATE_PAYLOAD);
 
-    expect(response.statusCode).toBe(200);
-  });
+		expect(response.statusCode).toBe(200);
+	});
 
-  test("delete Country", async () => {
-    const response = await request(app)
-                         .delete("/countries/4")
-                         .set("Authorization", `Bearer ${token}`);
+	test("delete Country", async () => {
+		const response = await request(app)
+			.delete("/countries/4")
+			.set("Authorization", `Bearer ${token}`);
 
-    expect(response.statusCode).toBe(200);
-  });
+		expect(response.statusCode).toBe(200);
+	});
 
-  afterAll(async () => { await logout(token).then((token = "")); });
+	afterAll(async () => {
+		await logout(token).then((token = ""));
+	});
 });
